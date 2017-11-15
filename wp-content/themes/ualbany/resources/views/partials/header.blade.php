bb1e713f4f1a663f292f921e66a217906b885917
Mat Jordan <mathew@verifiedstudios.com>
2017-11-15 13:46:23 -0500

    header

--- wp-content/themes/ualbany/resources/views/partials/header.blade.php
+++ wp-content/themes/ualbany/resources/views/partials/header.blade.php
@@ -1,10 +1,31 @@
 <header class="banner">
-  <div class="container">
-    <a class="brand" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a>
-    <nav class="nav-primary">
-      @if (has_nav_menu('primary_navigation'))
-        {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
-      @endif
-    </nav>
-  </div>
+    <section class="banner_utility">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12">
+                    <nav class="nav-utility">
+                        @if (has_nav_menu('utility_navigation'))
+                            {!! wp_nav_menu(['theme_location' => 'utility_navigation', 'menu_class' => 'nav']) !!}
+                        @endif
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="banner_primary">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-9 col-sm-3">
+                    <a class="brand" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a>
+                </div>
+                <div class="col-xs-3 col-sm-9">
+                    <nav class="nav-primary">
+                        @if (has_nav_menu('primary_navigation'))
+                            {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
+                        @endif
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </section>
 </header>
