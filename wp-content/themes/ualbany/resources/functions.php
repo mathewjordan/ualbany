0054ad594f74af792faded45e264cdc29bbc2dad
Mat Jordan <mathew@verifiedstudios.com>
2018-01-12 06:58:26 -0500

    submenu rendering

--- wp-content/themes/ualbany/resources/functions.php
+++ wp-content/themes/ualbany/resources/functions.php
@@ -60,6 +60,22 @@ array_map(function ($file) use ($sage_error) {
     }
 }, ['helpers', 'setup', 'plugins','filters', 'post_types', 'terradotta','admin','layouts']);
 
+function menu_set_dropdown( $sorted_menu_items, $args ) {
+    $last_top = 0;
+    foreach ( $sorted_menu_items as $key => $obj ) {
+        // it is a top lv item?
+        if ( 0 == $obj->menu_item_parent ) {
+            // set the key of the parent
+            $last_top = $key;
+        } else {
+            $sorted_menu_items[$last_top]->junk = 'yeah';
+            $sorted_menu_items[$last_top]->classes['dropdown'] = 'menu-dropdown';
+        }
+    }
+    return $sorted_menu_items;
+}
+add_filter( 'wp_nav_menu_objects', 'menu_set_dropdown', 10, 2 );
+
 /**
  * Here's what's happening with these hooks:
  * 1. WordPress initially detects theme in themes/sage/resources
