21f12a25eb22fbd519fbaecbdb78b96fe1124888
Mat Jordan <mathew@verifiedstudios.com>
2018-01-12 08:26:05 -0500

    styline of intro section

--- wp-content/themes/ualbany/resources/views/page.blade.php
+++ wp-content/themes/ualbany/resources/views/page.blade.php
@@ -38,95 +38,101 @@
     @while(have_posts()) @php(the_post())
         <article @php(post_class())>
             <?php if (get_field('page_introduction')) : ?>
-            <section class="container">
-                <div class="row">
-                    <?php
-                    $args = array(
-                        'post_type'      => 'page',
-                        'posts_per_page' => -1,
-                        'post_parent'    => $post->ID,
-                        'order'          => 'ASC',
-                        'orderby'        => 'menu_order'
-                    );
+            <section class="introduction-section">
+                <div class="container">
+                    <div class="row">
+                        <?php
+                        $args = array(
+                            'post_type'      => 'page',
+                            'posts_per_page' => -1,
+                            'post_parent'    => $post->ID,
+                            'order'          => 'ASC',
+                            'orderby'        => 'menu_order'
+                        );
 
-                    $parent = new WP_Query( $args );
-                    if ( $parent->have_posts() ) :
-                    $submenu = true;
+                        $parent = new WP_Query( $args );
+                        if ( $parent->have_posts() ) :
+                        $submenu = true;
 
-                    ?>
+                        ?>
 
-                    <aside class="col-sm-4">
-                        <div class="submenu--siblings">
-                            <ul>
-                                <li class="active-sibling"> <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><span class="fa fa-caret-right"></span> <?php the_title(); ?></a></li>
-                                <?php while ( $parent->have_posts() ) : $parent->the_post(); ?>
-                                    <li><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></li>
-                                <?php endwhile; ?>
-                            </ul>
-                        </div>
-                    </aside>
+                        <aside class="col-sm-4">
+                            <div class="submenu--siblings">
+                                <ul>
+                                    <li class="active-sibling"> <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><span class="fa fa-caret-right"></span> <?php the_title(); ?></a></li>
+                                    <?php while ( $parent->have_posts() ) : $parent->the_post(); ?>
+                                        <li><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></li>
+                                    <?php endwhile; ?>
+                                </ul>
+                            </div>
+                        </aside>
 
-                    <?php endif; wp_reset_postdata(); ?>
-                    <?php
+                        <?php endif; wp_reset_postdata(); ?>
+                        <?php
 
-                    $locations  = get_nav_menu_locations();
-                    $menu       = wp_get_nav_menu_object( $locations[ 'primary_navigation' ] );
-                    $menu_items = wp_get_nav_menu_items( $menu->term_id);
+                        $locations  = get_nav_menu_locations();
+                        $menu       = wp_get_nav_menu_object( $locations[ 'primary_navigation' ] );
+                        $menu_items = wp_get_nav_menu_items( $menu->term_id);
 
-                    $parent_id  = false;
-                    $parent     = '';
-                    $siblings   = '';
-                    $page_template = get_page_template_slug();
-                    $menu_post_id = $post->ID;
+                        $parent_id  = false;
+                        $parent     = '';
+                        $siblings   = '';
+                        $page_template = get_page_template_slug();
+                        $menu_post_id = $post->ID;
 
-                    if( !empty( $menu_items ) ) {
-                        // Find the menu item parent for this page
-                        foreach( $menu_items as $menu_item ) {
-                            if ($menu_item->object_id == $menu_post_id) {
-                                $parent_id = $menu_item->menu_item_parent;
-                                break;
-                            }
-                        }
-
-                        if ($parent_id) {
-
-                            $submenu = true;
-
-                            $siblings .= '<aside class="col-sm-4">';
-                            $siblings .= '<div class="submenu--siblings">';
-                            $siblings .= '<ul>';
-
-                            // render parent item
-                            $parent_item_post_id = get_post_meta($parent_id, '_menu_item_object_id', 1);
-                            $siblings .= '<li><a href="' . get_the_permalink($parent_item_post_id) . '">' . get_the_title($parent_item_post_id) . '</a></li>';
-
-                            // render children
+                        if( !empty( $menu_items ) ) {
+                            // Find the menu item parent for this page
                             foreach( $menu_items as $menu_item ) {
-                                if ($menu_item->menu_item_parent == $parent_id) {
-                                    if ($menu_item->object_id == $menu_post_id) {
-                                        $siblings .= '<li class="active-sibling"><a href="' . $menu_item->url . '"><span class="fa fa-caret-right"></span> ' . $menu_item->title . '</a></li>';
-                                    } else {
-                                        $siblings .= '<li><a href="' . $menu_item->url . '">' . $menu_item->title . '</a></li>';
-                                    }
+                                if ($menu_item->object_id == $menu_post_id) {
+                                    $parent_id = $menu_item->menu_item_parent;
+                                    break;
                                 }
                             }
-                            $siblings .= '</ul>';
-                            $siblings .= '</div>';
-                            $siblings .= '</aside>';
 
-                            echo $parent . $siblings;
+                            if ($parent_id) {
+
+                                $submenu = true;
+
+                                $siblings .= '<aside class="col-sm-4">';
+                                $siblings .= '<div class="submenu--siblings">';
+                                $siblings .= '<ul>';
+
+                                // render parent item
+                                $parent_item_post_id = get_post_meta($parent_id, '_menu_item_object_id', 1);
+                                $siblings .= '<li><a href="' . get_the_permalink($parent_item_post_id) . '">' . get_the_title($parent_item_post_id) . '</a></li>';
+
+                                // render children
+                                foreach( $menu_items as $menu_item ) {
+                                    if ($menu_item->menu_item_parent == $parent_id) {
+                                        if ($menu_item->object_id == $menu_post_id) {
+                                            $siblings .= '<li class="active-sibling"><a href="' . $menu_item->url . '"><span class="fa fa-caret-right"></span> ' . $menu_item->title . '</a></li>';
+                                        } else {
+                                            $siblings .= '<li><a href="' . $menu_item->url . '">' . $menu_item->title . '</a></li>';
+                                        }
+                                    }
+                                }
+                                $siblings .= '</ul>';
+                                $siblings .= '</div>';
+                                $siblings .= '</aside>';
+
+                                echo $parent . $siblings;
+                            }
                         }
-                    }
-                    ?>
-                    <?php if ($submenu) : ?>
-                        <div class="col-sm-8">
-                            <?php echo get_field('page_introduction_content');?>
-                        </div>
-                    <?php else: ?>
-                        <div class="col-12">
-                            <?php echo get_field('page_introduction_content');?>
-                        </div>
-                    <?php endif; ?>
+                        ?>
+                        <?php if ($submenu) : ?>
+                            <div class="col-sm-8">
+                                <div class="introduction-section--content">
+                                    <?php echo get_field('page_introduction_content');?>
+                                </div>
+                            </div>
+                        <?php else: ?>
+                            <div class="col-12">
+                                <div class="introduction-section--content">
+                                    <?php echo get_field('page_introduction_content');?>
+                                </div>
+                            </div>
+                        <?php endif; ?>
+                    </div>
                 </div>
             </section>
             <?php endif; ?>
