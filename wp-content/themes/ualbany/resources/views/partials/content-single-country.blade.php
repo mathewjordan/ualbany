0734819e5be492b1c1e87f9db8a46294403b5322
Barrett Cox <barrettcox80@gmail.com>
2018-04-16 13:07:03 -0700

    Columns for program listing

--- wp-content/themes/ualbany/resources/views/partials/content-single-country.blade.php
+++ wp-content/themes/ualbany/resources/views/partials/content-single-country.blade.php
@@ -1,42 +1,91 @@
 <div class="container">
   <article @php(post_class())>
     <header>
-      <h1 class="entry-title">Programs in {{ get_the_title() }}</h1>
+      <h1 class="sr-only">{{ get_the_title() }} Programs</h1>
     </header>
     <div class="entry-content">
-      @php
+      <section>
+        <header>
+          <h2>Featured {{ get_the_title() }} Programs</h2>
+        </header>
+        @php
 
-      the_content();
-
-      $args = [
-                'post_type' => 'program',
-                'order'     => 'ASC',
-                'orderby'   => 'title',
-                'posts_per_page' => -1,
-                'meta_query' => [
-                  [
-                    'key'     => 'program_country',
-                    'value'   => get_the_ID(),
-                    'compare' => '=='
+        $args = [
+                  'post_type' => 'program',
+                  'order'     => 'ASC',
+                  'orderby'   => 'title',
+                  'posts_per_page' => -1,
+                  'meta_query' => [
+                    [
+                      'key'     => 'program_country',
+                      'value'   => get_the_ID(),
+                      'compare' => '=='
+                    ]
                   ]
-                ]
-              ];
+                ];
 
-      $query = new WP_Query($args);
+        $query = new WP_Query($args);
 
-      @endphp
-                      
-      @if ($query->have_posts())
-        <article @php(post_class())>
-        @while($query->have_posts()) @php($query->the_post())
-          <a href="{{ get_the_permalink() }}">
-            <h2>{{ get_the_title() }}</h2>
-          </a>
-        @endwhile
-        </article>
-        @php(wp_reset_postdata())
-      @endif
+        @endphp
 
+        @if ($query->have_posts())
+          @while($query->have_posts()) @php($query->the_post())
+          <article @php(post_class())>
+            <div class="row">
+            <div class="col-sm-2">
+              Lorem ipsum
+            </div>
+            <div class="col-sm-7">
+              <a href="{{ get_the_permalink() }}">
+                <h2>{{ get_the_title() }}</h2>
+              </a>
+              Lorem ipsum
+            </div>
+            <div class="col-sm-3">
+              Lorem ipsum
+            </div>
+            </div>
+          </article>
+          @endwhile
+          @php(wp_reset_postdata())
+        @endif
+      </section>
+      <section>
+        <header>
+          <h2>All {{ get_the_title() }} Programs</h2>
+        </header>
+        @php
+
+        $args = [
+                  'post_type' => 'program',
+                  'order'     => 'ASC',
+                  'orderby'   => 'title',
+                  'posts_per_page' => -1,
+                  'meta_query' => [
+                    [
+                      'key'     => 'program_country',
+                      'value'   => get_the_ID(),
+                      'compare' => '=='
+                    ]
+                  ]
+                ];
+
+        $query = new WP_Query($args);
+
+        @endphp
+
+        @if ($query->have_posts())
+          <article @php(post_class())>
+          @while($query->have_posts()) @php($query->the_post())
+            <a href="{{ get_the_permalink() }}">
+              <h2>{{ get_the_title() }}</h2>
+            </a>
+          @endwhile
+          </article>
+          @php(wp_reset_postdata())
+        @endif
+      </section>
+      
     </div>
     <footer>
       {!! wp_link_pages(['echo' => 0, 'before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']) !!}
