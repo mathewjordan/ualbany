23d564b62c90d07a2c37b440328e2d67d5d1344c
Barrett Cox <barrettcox80@gmail.com>
2018-05-04 14:57:26 -0700

    Mobile Menu

--- wp-content/themes/ualbany/resources/assets/scripts/routes/common.js
+++ wp-content/themes/ualbany/resources/assets/scripts/routes/common.js
@@ -1,7 +1,11 @@
+import MobileNav from '../components/MobileNav'
+
 export default {
   init() {
     // JavaScript to be fired on all pages
     $('.program-slides').slick({  dots: true });
+    
+    new MobileNav();
   },
   finalize() {
     // JavaScript to be fired on all pages, after page specific JS is fired
